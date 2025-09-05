afa3f54578e1d3ad356ed2b4154bf47a3415aa3f
ohbeomho <ohbeomho0322@gmail.com>
2025-09-05 23:01:05 +0900

    두 직선의 교점 표시

--- 직선의-교점/index.js
+++ 직선의-교점/index.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+ctx.font = "16px monospace";
+ctx.textAlign = "center";
+
 // 직선
 const lines = [
 	{ a: 1, b: 0, color: "red" },
@@ -12,7 +15,14 @@ function coordTransform(x, y) {
 	return [canvas.width / 2 + x, canvas.height / 2 - y];
 }
 
-// TODO: 두 직선의 교점 구하고 화면에 표시
+function getIntersection(l1, l2) {
+	const adiff = l1.a - l2.a,
+		bdiff = l1.b - l2.b;
+	const x = -(bdiff / adiff),
+		y = l1.a * x + l1.b;
+
+	return [x, y];
+}
 
 function drawLine(a, b, color) {
 	const sx = -canvas.width / 2,
@@ -29,7 +39,7 @@ function drawLine(a, b, color) {
 	ctx.strokeStyle = prevColor;
 }
 
-function drawLines() {
+function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	ctx.globalAlpha = 0.4;
@@ -44,6 +54,14 @@ function drawLines() {
 
 	// 직선 그리기
 	for (let line of Object.values(lines)) drawLine(line.a, line.b, line.color);
+
+	// 교점 표시
+	const [x, y] = getIntersection(...lines);
+	ctx.beginPath();
+	ctx.arc(...coordTransform(x, y), 2, 0, 2 * Math.PI);
+	ctx.fill();
+
+	ctx.fillText(`(${x}, ${y})`, ...coordTransform(x, y + 10));
 }
 
 const inputs = Array.from(document.querySelectorAll("input"));
@@ -53,8 +71,8 @@ for (let input of inputs) {
 
 	input.addEventListener("input", () => {
 		lines[line][target] = input.valueAsNumber;
-		drawLines();
+		draw();
 	});
 }
 
-drawLines();
+draw();
