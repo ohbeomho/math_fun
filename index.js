1ba19821952b88c75e272a4da89349537d7dcbd1
ohbeomho <ohbeomho0322@gmail.com>
2025-09-27 21:49:35 +0900

    Home page

---
+++ index.js
@@ -0,0 +1,12 @@
+const pages = [
+	'두-원의-교점-지나는-직선',
+	'원의-접선',
+	'직선과-이차함수-거리',
+	'직선의-교점',
+]
+for (let page of pages) {
+	const a = document.createElement('a')
+	a.href = `/${page}/`
+	a.textContent = page.split('-').join(' ')
+	document.body.append(a, document.createElement('br'))
+}
