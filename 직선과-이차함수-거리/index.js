27835bbfeadf74f285791942d845726f1ecba496
ohbeomho <ohbeomho0322@gmail.com>
2025-09-27 17:51:54 +0900

    TODO: 이차함수 그래프와 직선 거리

---
+++ 직선과-이차함수-거리/index.js
@@ -0,0 +1,12 @@
+import { coordTransform } from "../coord.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// https://www.mathcha.io/editor/zGw9JCElSdyhjmo81XHE7yynCpl1Glgc6Wmj5X
+const quad_func = { a: 1, b: 30, c: 20 };
+const line = { m: -2, n: -40 };
+
+function getDistance(f, l) {
+	// TODO: 이차함수 그래프와 직선의 길이 구하기
+}
