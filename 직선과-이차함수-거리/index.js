9ec1856bceae20910aa49f612cdc163e7b2619e3
ohbeomho <ohbeomho0322@gmail.com>
2025-09-28 23:06:29 +0900

    거리를 선분으로 나타내기

--- 직선과-이차함수-거리/index.js
+++ 직선과-이차함수-거리/index.js
@@ -43,6 +43,23 @@ function drawQuadFuncGraph({ a, p, q }) {
 	}
 }
 
+// 점 p를 지나고 직선 l과 수직인 직선 구하기
+function getSuzikLine(l, p) {
+	const { m } = l
+	const { x, y } = p
+	return { m: -1 / m, n: y - (-1 / m) * x }
+}
+
+// 두 직선 l1, l2의 교점 구하기
+function getIntersection(l1, l2) {
+	const mdiff = l1.m - l2.m,
+		ndiff = l1.n - l2.n
+	const x = -(ndiff / mdiff),
+		y = l1.m * x + l1.n
+
+	return [x, y]
+}
+
 function update() {
 	const [distance, k] = getDistance(quad_func, line)
 
@@ -51,8 +68,20 @@ function update() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 	// x축, y축
-	draw(ctx, 'line', { x1: 0, y1: -canvas.height, x2: 0, y2: canvas.height })
-	draw(ctx, 'line', { x1: -canvas.width, y1: 0, x2: canvas.width, y2: 0 })
+	draw(ctx, 'line', {
+		x1: 0,
+		y1: -canvas.height,
+		x2: 0,
+		y2: canvas.height,
+		alpha: 0.5,
+	})
+	draw(ctx, 'line', {
+		x1: -canvas.width,
+		y1: 0,
+		x2: canvas.width,
+		y2: 0,
+		alpha: 0.5,
+	})
 
 	// 직선
 	drawLine(line)
@@ -62,12 +91,16 @@ function update() {
 	drawQuadFuncGraph(quad_func)
 
 	// y = mx + k 와 y = f(x) 의 교점
-	const { a, p, q } = quad_func
-	const x = (2 * a * p + line.m) / 2,
-		y = line.m * x + k
-	draw(ctx, 'dot', { x, y })
+	const { a, p } = quad_func
+	const x1 = (2 * a * p + line.m) / 2,
+		y1 = line.m * x1 + k
 
-	// TODO: y = mx + k 와 y = mx + n 의 거리를 선분으로 표시하기
+	// 그 교점을 지나는 y = mx + k 와 수직인 직선과 y = mx + n 의 교점
+	const suzikLine = getSuzikLine(line, { x: x1, y: y1 })
+	const [x2, y2] = getIntersection(suzikLine, line)
+
+	// 위 두 점을 이은 선분
+	draw(ctx, 'line', { x1, y1, x2, y2, color: 'green' })
 }
 
 update()
